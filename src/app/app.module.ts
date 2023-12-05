123fe969f933dd7e7e489a95e7bbb73b8c4f94c9
Augusto Acual <augustoacual@gmail.com>
2023-12-05 11:46:56 -0600

    Two way data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component'
 import { PostCreateComponent } from './posts/post-crete/post-create.component'
 
 @NgModule({
   declarations: [AppComponent, PostCreateComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
