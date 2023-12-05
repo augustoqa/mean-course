9a62d15ff44d7a1baf8ec2558e11cf86b2a3ed07
Augusto Acual <augustoacual@gmail.com>
2023-12-05 12:58:34 -0600

    Create header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { FormsModule } from '@angular/forms'
 import { MatInputModule } from '@angular/material/input'
 import { MatCardModule } from '@angular/material/card'
 import { MatButtonModule } from '@angular/material/button'
+import { MatToolbarModule } from '@angular/material/toolbar'
 
 import { AppComponent } from './app.component'
 import { PostCreateComponent } from './posts/post-crete/post-create.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { HeaderComponent } from './header/header.component'
 
 @NgModule({
-  declarations: [AppComponent, PostCreateComponent],
+  declarations: [AppComponent, PostCreateComponent, HeaderComponent],
   imports: [
     BrowserModule,
     FormsModule,
@@ -18,6 +20,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
     MatInputModule,
     MatCardModule,
     MatButtonModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
