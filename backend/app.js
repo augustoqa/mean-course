893135131ac53aadfb10938445a673cf58fc7fda
Augusto Quisquinay <augustoacual@gmail.com>
2024-06-08 12:07:28 -0600

    Create Post Model using Mongoose and Connect to the DB

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+const Port = require('./models/post')
 
 const app = express()
+
+mongoose
+  .connect(
+    'mongodb+srv://mean:hnusDzKhO7w1wq8z@cluster0.vrn9p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+  )
+  .then(() => console.log('Connected to the database'))
+  .catch(() => console.log('Connection failed!'))
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
@@ -32,6 +42,17 @@ app.use('/api/posts', (req, res, next) => {
   res.status(200).json({
     message: 'Posts fetched successfully!',
     posts,
+  })
+})
+
+app.post('/api/posts', (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  })
+  console.log(post)
+  res.status(201).json({
+    message: 'Post added successfully',
   })
 })
 
